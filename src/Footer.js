bb5d4157ee1cfe5892488121e60d224f6093290d
Karsten Rogers <karstenrogers@Karstens-Air.localdomain>
2018-09-25 08:51:16 -0400

    reactified-octocats

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Footer extends Component {
+    render() { 
+        return ( 
+            <div>
+                <footer>
+    <p class="footer-img"><img src="https://octodex.github.com/ui/logo.png"/></p>
+  </footer>
+            </div>
+         );
+    }
+}
+ 
+export default Footer;
